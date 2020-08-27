ef5bd4b65aebe8d2768b2404478e7b746da29608
Rafael Souza <skotpipen@gmail.com>
2020-08-26 22:18:05 -0300

    Loop, Laços

---
+++ aula02/ex_03.py
@@ -0,0 +1,14 @@
+# Loop, Laçoes e repetiçoes
+
+# while
+## -> Variavel de controle
+## -> Ritmo de repetição ou passo
+## -> Condição de parada
+
+# for => foreach
+
+for num in range(0,10,1): # retorna um intervalo (0,1,2,3,4,5,6,7,8,9)
+    print(num)
+
+
+
