bfe6da71afefaeb0ff88744f4057706d8ea5ae08
Rafael Souza <skotpipen@gmail.com>
2020-08-26 21:05:03 -0300

    exercicios lista tuplas e dicionarios

---
+++ aula02/ex_02.py
@@ -0,0 +1,22 @@
+
+# Lista - Mutaveis
+
+lista = []
+lista = ['String', 23.5, True, [0,1,2]]
+
+
+# Dupla - nao mutaveis
+
+tuplas = ()
+
+tuplas = ('String', 23.5, True)
+
+
+#Dicionario - Mutavel
+
+dicionarios = {}
+
+dicionarios = {'nome':'Rafael','idade':37,'nacionalidade':'brasileira'}
+
+print (dicionarios['nome'])
+
