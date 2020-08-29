cb43c6e88e8f420d30ead88d25c8d3afe88b064c
Rafael Souza <skotpipen@gmail.com>
2020-08-28 22:03:14 -0300

    modulos

---
+++ aula04/ex_01.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+# Modulos
+
+## a partir da biblioteca nativa
+## random, time, sys, os, re
+import sys
+
+# Pega o argumento de comando
+print(f'Este é um argumento de linha de comando {sys.argv[1]}')
+
